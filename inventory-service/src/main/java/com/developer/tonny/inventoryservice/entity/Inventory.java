329e5e3abdae325137fae858c2e39f434b589226
Tonny Oseko <tonnyoseko0@gmail.com>
2024-03-28 02:44:07 +0300

    create a inventory entity, and repository

---
+++ inventory-service/src/main/java/com/developer/tonny/inventoryservice/entity/Inventory.java
@@ -0,0 +1,25 @@
+package com.developer.tonny.inventoryservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tbl_inventories")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_code")
+    private String productCode;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+}
